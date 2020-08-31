56fce0c561ac5f456e242e7d0e56cd5062a15244
Matteo Penna <matteo.penna@outlook.com>
2020-08-31 18:22:48 +0200

    Added gRPC client

---
+++ src/gRPCClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Net.Client;
+using StudentProtos;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new StudentService.StudentServiceClient(channel);
+
+            while (true)
+            {
+                Console.WriteLine("Enter an argument");
+
+                string line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    break;
+                }
+
+                var reply = await client.GetStudentAsync(
+                    new GetStudentRequest() { Id = line }
+                );
+
+                if (reply.Student != null)
+                {
+                    Console.WriteLine("Reply: " + reply.Student);
+                }
+
+                if (reply.Error != null)
+                {
+                    Console.WriteLine("Reply: " + reply.Error);
+                }
+            }
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
